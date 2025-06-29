b454d6e8a2cb322a12b4b57afe9a867b2bd751a9
kayden14 <faithtega14@gmail.com>
2025-06-29 12:01:06 +0100

    Initial commit from alx-listing-app-00

---
+++ alx-listing-app-00-master/next.cnfig.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+};
+
+module.exports = nextConfig;
+// This configuration file for a Next.js application specifies that images can be loaded from the domain "images.unsplash.com".
+// It allows the application to optimize and serve images from this domain efficiently. 
